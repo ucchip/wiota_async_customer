1d407b827a199ff2b01b6a0212cfbdccc6e64720
jingping.wang <1182784514@qq.com>
2022-09-16 18:26:00 +0800

    del .pub

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -3,7 +3,7 @@
 #define _UC_WIOTA_VERSION_H__
 
 #define GIT_TAIL_TAG   "v0.02_async"
-#define GIT_UPDATE_VERSION_TIME  "Fri Sep 16 16:26:49 2022"
+#define GIT_UPDATE_VERSION_TIME  "Fri Sep 16 18:15:30 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
