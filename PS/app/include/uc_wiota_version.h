c8a005d2efc455ea6cfb7e1472bc3530177d458b
zjj609 <zjj609@qq.com>
2023-06-03 10:58:37 +0800

    version 2.8 tag

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v2.7_async"
-#define GIT_UPDATE_VERSION_TIME  "Mon Mar 6 15:11:42 2023"
+#define GIT_TAIL_TAG   "v2.8_async"
+#define GIT_UPDATE_VERSION_TIME  "Sat Jun 3 10:57:52 2023"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
