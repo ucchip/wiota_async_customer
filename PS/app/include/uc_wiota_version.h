18d8d5b12e067f1a6b6e7b919bf7a2cf40a30cc9
zjj609 <zjj609@qq.com>
2023-12-26 11:16:54 +0800

    update version info and tag.

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v3.01_async"
-#define GIT_UPDATE_VERSION_TIME  "Thu Oct 26 14:26:07 2023"
+#define GIT_TAIL_TAG   "v3.02_async"
+#define GIT_UPDATE_VERSION_TIME  "Tue Dec 26 11:13:36 2023"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
