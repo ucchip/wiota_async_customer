3ec0d442679011c182c02159b8a81d7168dc3383
zjj609 <zjj609@qq.com>
2024-06-07 17:32:52 +0800

    update version tag

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v3.03_async"
-#define GIT_UPDATE_VERSION_TIME  "Wed May 22 11:50:57 2024"
+#define GIT_TAIL_TAG   "v4.0_async"
+#define GIT_UPDATE_VERSION_TIME  "Fri Jun 7 17:31:26 2024"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
