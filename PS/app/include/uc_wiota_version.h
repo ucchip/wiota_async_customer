dba7d4c3c87a99588b9c7d69a2c5064c60c9e41d
ucchip <zheng.wang@ucchip.com>
2026-03-13 16:12:32 +0800

    update version.

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v4.3_async"
-#define GIT_UPDATE_VERSION_TIME  "Mon Sep 15 11:11:10 2025"
+#define GIT_TAIL_TAG   "v4.4_async"
+#define GIT_UPDATE_VERSION_TIME  "Fri Mar 13 16:10:55 2026"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
